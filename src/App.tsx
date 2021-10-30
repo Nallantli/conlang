f338dded1590bb5186dd2514cf7f195bdfc9239e
Benjamin Park <parkbenjamin2@gmail.com>
2021-10-30 19:10:37 -0400

    no router

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,14 @@
-import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { Glosser } from "./Glosser";
 import styles from './darkTheme.module.scss';
 import { Search } from "./Search";
 
 export default function App() {
 	const urlParams = new URLSearchParams(window.location.search);
-	return (
-		<Router>
-			<Switch>
-				<Route exact path="/">
-					<Glosser query={urlParams.get('q')?.split('|')} styles={styles} />
-				</Route>
-				<Route path="/search">
-					<Search styles={styles} query={urlParams.get('q') || ''} />
-				</Route>
-			</Switch>
-		</Router>
-	);
+	console.log(window.location.hash);
+	switch (window.location.hash) {
+		case "#/search":
+			return <Search styles={styles} query={urlParams.get('q') || ''} />;
+		default:
+			return <Glosser query={urlParams.get('q')?.split('|')} styles={styles} />;
+	}
 }
